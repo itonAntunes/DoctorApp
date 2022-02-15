5230ecb7fef45b0f3730adfaa02a0fa5e965cc24
Everton Antunes <iton.antunes@gmail.com>
2022-02-15 00:50:57 -0300

    Layout profile parcialmente completo

---
+++ app/src/main/java/com/example/doctorapp/ForgotPasswordDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.example.doctorapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ForgotPasswordDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password_dialog, container, false)
+    }
+
+    companion object {
+        const val TAG = "ForgotPasswordDialog"
+    }
+}
--- app/src/main/java/com/example/doctorapp/MainActivity.kt
+++ app/src/main/java/com/example/doctorapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.doctorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+
+    private val btnDialog: Button
+        get() = findViewById(R.id.btn_dialog)
+
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dialog = ForgotPasswordDialogFragment()
+        btnDialog.setOnClickListener {
+            dialog.show(supportFragmentManager, ForgotPasswordDialogFragment.TAG)
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
     }
 }
